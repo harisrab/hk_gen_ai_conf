90046cab690eae61d61788922e908a78356cf951
Haris Rashid <62747193+harisrab@users.noreply.github.com>
2023-04-22 11:21:31 +0000

    Done

--- src/components/EventLocation.tsx
+++ src/components/EventLocation.tsx
@@ -9,9 +9,9 @@ const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const EventLocation = () => {
   return (
     <main
-      className={`w-full overflow-hidden  ${inter.variable} flex flex-col items-center justify-between relative bg-green-800/[0.05]`}
+      className={`w-full overflow-hidden  ${inter.variable} flex flex-col items-center justify-between relative bg-green-800/[0.05] mt-10 sm:mt-0`}
     >
-      <div className="z-10  w-full h-screen flex justify-between selection:text-[#76EDCB] selection:bg-[#0F1F21] items-center">
+      <div className="z-10  w-full h-screen flex flex-col justify-between selection:text-[#76EDCB] selection:bg-[#0F1F21] items-center sm:flex-row">
         <div className="px-10 sm:px-20 selection:text-[#76EDCB] selection:bg-[#0F1F21] w-full">
           <h1 className="font-medium text-[1.55rem] sm:text-[3rem] text-transparent bg-clip-text leading-[45px]  sm:leading-[70px] bg-gradient-to-r to-[#83878E] from-white">
             Unravel brilliance at The Mills
@@ -26,7 +26,7 @@ const EventLocation = () => {
           </p>
         </div>
         <div className="w-full h-full flex items-center justify-end">
-          <div className="relative w-[600px] mr-10 h-[600px] opacity-100 hover:opacity-50 duration-200 flex items-center">
+          <div className="relative w-full sm:mr-10 h-[600px] opacity-100 hover:opacity-50 duration-200 flex items-center">
             <Image
               alt=""
               src="/mills2.jpeg"
