754ddf3fa34e4a8eaeabc78ff0352e951808fa69
Haris Rashid <harisrab01@gmail.com>
2023-05-18 11:54:05 +0800

    Done

--- src/components/LogoMarquee.tsx
+++ src/components/LogoMarquee.tsx
@@ -3,7 +3,7 @@ import Marquee from "react-fast-marquee";
 
 const LogoMarquee = () => {
   return (
-    <footer className="w-full h-[140px]  bg-green-800/[0.05] mt-10 sm:mt-0 mb-10 sm:mb-0">
+    <footer className="w-full h-[140px] items-center flex  bg-green-800/[0.05] mt-10 sm:mt-0 mb-10 sm:mb-0">
       <Marquee className="h-full flex items-center" gradient={false}>
         {/* <div className="flex items-center gap-2 h-full"> */}
         <div className="relative w-[200px] mr-10 h-[60px] opacity-40 hover:opacity-50 duration-200 flex items-center">
